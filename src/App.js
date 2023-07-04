9082c1390e570579993475e5e77105c193a8ed9a
AnoopAG91 <anoopag47@gmail.com>
2023-07-04 13:34:14 +0530

    Gym website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/home/home';
+import About from './components/pages/about/about' 
+import Classes from './components/pages/classes/classes'
+import Contact from './components/pages/contact/contact'
+import SignUp from './components/signUp/signUp';
+import Login from './components/login/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/classes" element={<Classes/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path='/signUp' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+      </Routes>
+      </BrowserRouter>
+  
+   </div>
   );
 }
 
